fe75919f1081a36b93c3bc181fa44351590c7b4e
Tom Grek <tom.grek@gmail.com>
2020-01-16 20:52:08 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Game Of Life (John Conway) with convolutions, in PyTorch
+
+Cellular automata... in PyTorch, using convolutions.
+
+Just a fun thing to try.
--- life.py
+++ life.py
@@ -1,41 +1,33 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 import torch
 import torch.nn.functional as F
 
 from PIL import Image
 
-plt.ion()
+BOARD_HEIGHT = 10
+BOARD_WIDTH = 10
+
+distrib = torch.distributions.Bernoulli(0.4)
 
 weights = torch.tensor([[1,1,1],[1,10,1],[1,1,1]]).view(1,1,3,3) 
-board = torch.tensor([[0,0,0,0,0], [0,0,0,0,0],[0,1,1,1,0],[0,0,0,0,0],[0,0,0,0,0]])
+#board = torch.tensor([[0,0,0,0,0], [0,0,0,0,0],[0,1,1,1,0],[0,0,0,0,0],[0,0,0,0,0]]).view(1,1,5,5)
+board = distrib.sample((BOARD_HEIGHT,BOARD_WIDTH)).view(1,1,BOARD_HEIGHT,BOARD_WIDTH)
+board = board.to(torch.int64)
 
-b = board.view(1,1,5,5)
+cv2.namedWindow("game", cv2.WINDOW_NORMAL) # WINDOW_AUTOSIZE doesn't have the numbers!
 
-t = F.conv2d(b, weights, padding=1).view(5,5)
-t = (t==12) | (t==3) | (t==13)
-npt = np.int8(t) * 255
-cv2.namedWindow("game", cv2.WINDOW_AUTOSIZE)
-
-img = Image.fromarray(npt).convert('RGB')
-img = np.array(img)
-
-cv2.imshow("game", img)
-
-#import ipdb; ipdb.set_trace()
-cv2.waitKey(1000)
-# import time
-# time.sleep(2)
-
-b = torch.tensor(t, dtype=torch.int64).view(1,1,5,5)
-
-t = F.conv2d(b, weights, padding=1).view(5,5)
-t = (t==12) | (t==3) | (t==13)
-npt = np.int8(t) * 255
-img = Image.fromarray(npt).convert('RGB')
-img = np.array(img)
-cv2.imshow("game", img)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+while True:
+    newboard = F.conv2d(board, weights, padding=1).view(BOARD_HEIGHT,BOARD_WIDTH)
+    newboard = (newboard==12) | (newboard==3) | (newboard==13)
+    newboard_array = np.int8(newboard) * 255
+    img = Image.fromarray(newboard_array).convert('RGB')
+    img = np.array(img)
+    cv2.imshow("game", img)
+    q = cv2.waitKey(500)
+    if q == 113: # 'q'
+        cv2.destroyAllWindows()
+        sys.exit(0)
+    board = torch.tensor(newboard_array/255, dtype=torch.int64).view(1,1,BOARD_HEIGHT,BOARD_WIDTH)
